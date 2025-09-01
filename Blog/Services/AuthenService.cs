3be35d441e883d978dfeda5b292a9cdd95ecedb0
thinh <thinhqn999@gmail.com>
2025-09-01 22:00:19 +0700

    phần 4

--- Blog/Services/AuthenService.cs
+++ Blog/Services/AuthenService.cs
@@ -1,21 +1,67 @@
 ﻿using Blog.ViewModel;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Blog.Services
 {
   public class AuthenService
   {
     private readonly UserService _userService;
+    private readonly ProtectedLocalStorage _protectedLocalStorage;
 
-    public AuthenService(UserService userService)
+    public AuthenService(UserService userService, ProtectedLocalStorage protectedLocalStorage)
     {
       _userService = userService;
+      _protectedLocalStorage = protectedLocalStorage;
     }
 
     public async Task<UserLogin?> GetUserAsync(LoginViewModel loginViewModel)
     {
       var userLogin = await _userService.LoginAsync(loginViewModel);
+
+      if (userLogin is not null)
+      {
+        await SaveInfoUserToBrowserStorageAsync(userLogin.Value);
+      }
+
       return userLogin;
     }
+
+    private const string UserStorageKey = "blog_user";
+    //private JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions();
+    private readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions
+    {
+
+    };
+
+    public async Task SaveInfoUserToBrowserStorageAsync(UserLogin userLogin) =>
+      await _protectedLocalStorage
+      .SetAsync(UserStorageKey, JsonSerializer
+        .Serialize(userLogin, _jsonSerializerOptions));
+
+    public async Task<UserLogin?> GetInfoUserToBrowserStorageAsync()
+    {
+      try
+      {
+        var result = await _protectedLocalStorage.GetAsync<string>(UserStorageKey);
+
+        if (result.Success && !string.IsNullOrWhiteSpace(result.Value))
+        {
+          var userLogin = JsonSerializer.Deserialize<UserLogin>(result.Value, _jsonSerializerOptions);
+          return userLogin;
+        }
+
+      }
+      catch (Exception)
+      {
+        throw;
+      }
+      return null;
+    }
+
+    public async Task RemoveInfoUserToBrowserStorageAsync() =>
+      await _protectedLocalStorage.DeleteAsync(UserStorageKey);
 
   }
 }
--- Blog/Services/BlogAuthStateProvider.cs
+++ Blog/Services/BlogAuthStateProvider.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
+﻿using Blog.ViewModel;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
 
 namespace Blog.Services
 {
   public class BlogAuthStateProvider : AuthenticationStateProvider
   {
+    // hằng số
+    private const string BlogAuthenKey = "blog_authen";
     // tạo hàm dựng constructor
     private readonly AuthenService _authenService;
 
@@ -12,14 +16,60 @@ namespace Blog.Services
       _authenService = authenService;
     }
 
-    public override Task<AuthenticationState> GetAuthenticationStateAsync()
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
     {
-      throw new NotImplementedException();
+      var claimsPrincipal = new ClaimsPrincipal();
+
+      var user = await _authenService.GetInfoUserToBrowserStorageAsync();
+      if (user is not null)
+      {
+        claimsPrincipal = GetClaimsPrincipalFromUser(user.Value);
+      }
+
+      var authenState = new AuthenticationState(claimsPrincipal);
+      NotifyAuthenticationStateChanged(Task.FromResult(authenState));
+      return authenState;
+
+    }
+
+    public async Task<string> LoginAsync(LoginViewModel loginViewModel)
+    {
+      var userLogin = await _authenService.GetUserAsync(loginViewModel);
+
+      if (userLogin is null)
+      {
+        return "Email đăng nhập hoặc mật khẩu không đúng";
+      }
+
+      var authenState = new AuthenticationState(GetClaimsPrincipalFromUser(userLogin.Value));
+      NotifyAuthenticationStateChanged(Task.FromResult(authenState));
+      return null;
+    }
+
+    public async Task LogoutAsync()
+    {
+      await _authenService.RemoveInfoUserToBrowserStorageAsync();
+      var authenState = new AuthenticationState(new ClaimsPrincipal());
+      NotifyAuthenticationStateChanged(Task.FromResult(authenState));
     }
 
     //public override async Task<AuthenticationState> GetAuthenticationStateAsync()
     //{
     //  var user = await _authenService.GetUserAsync();
     //}
+
+    private static ClaimsPrincipal GetClaimsPrincipalFromUser(UserLogin userLogin)
+    {
+      var identity = new ClaimsIdentity(
+        new[]
+        {
+          new Claim(ClaimTypes.NameIdentifier, userLogin.UserId.ToString()),
+          new Claim(ClaimTypes.Name, userLogin.DisplayName)
+        },
+        BlogAuthenKey
+        );
+      return new ClaimsPrincipal(identity);
+    }
+
   }
 }
